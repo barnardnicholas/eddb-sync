67e6567b8abdeb9cdaf79a287c86b03a905637c1
Nick Barnard <55743033+barnardnicholas@users.noreply.github.com>
2022-11-09 16:51:12 +0000

    Init commit - add all scripts

---
+++ README.md
@@ -0,0 +1,13 @@
+# eddb-sync
+ Sync Data from EDDB for Ship Shopper
+
+ ## Scripts
+ `npm run fetch`
+ Fetch raw data from EDDB and cache to local files
+
+ `npm run process`
+Process cached data to minify
+
+`npm run full`
+Both of the above
+
---
+++ refetch-data.js
@@ -0,0 +1,18 @@
+const {
+  fetchLiveModules,
+  fetchLivePopulatedSystems,
+  fetchLiveStations,
+} = require('./fetches');
+
+const downloadData = async () => {
+  try {
+    await fetchLiveModules();
+    await fetchLivePopulatedSystems();
+    await fetchLiveStations();
+  } catch(e) {
+    console.error(e)
+  }
+  console.log('FINISHED')
+} // Download fersh data from EDDB
+
+downloadData(); // Download fresh data from EDDB
